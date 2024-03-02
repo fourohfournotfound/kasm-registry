93fa0b9362598b7795ece6b432f26483cd73dc7f
fourohfournotfound <51336043+fourohfournotfound@users.noreply.github.com>
2024-03-01 18:26:52 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Local Kasm ML',
+    description: 'Repository to use my own images in kasm',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://fourohfournotfound/kasm-registry',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
